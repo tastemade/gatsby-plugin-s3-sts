cd5bf3567313b6bf2cb4a8cff3da037d8f50a08e
Steven Simonitch <ssimonitch@gmail.com>
2022-08-05 15:13:06 +0900

    restructure

--- examples/with-redirects/README.md
+++
@@ -1,3 +0,0 @@
-# with-redirects
-
-A very simple example showing off redirects of 2 types: normal ones and root redirects (which require a workaround that gatsby-plugin-s3 applies) & client only routes.
---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bin_1 = require("./bin");
+Object.defineProperty(exports, "deploy", { enumerable: true, get: function () { return bin_1.deploy; } });
+//# sourceMappingURL=index.js.map
